e426e47d1aa210cc87774f13735e1a4624721d1a
Prasansa05 <guptaprasansa@gmail.com>
2025-09-26 21:34:00 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,12 @@
+#using for loop
+n= int(input("Enter a Number"))
+for i in range(1,11):
+    t=n*i
+    print(t)
+#using while loop
+num=int(input("Enter the number whose table is to be printed"))
+i=1
+while(i<=10):
+    t=num*i
+    i+=1
+    print(t)
