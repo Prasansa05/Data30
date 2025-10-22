bbc7f1a87f674e8e17bc36ace19ee02b8778e174
Prasansa05 <guptaprasansa@gmail.com>
2025-10-23 02:39:50 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,8 @@
+class Student:
+ def __init(self,name,marks):
+    self.name=name
+    self.marks=marks
+ def average(self,a,b,c,d,e):
+    return (a+b+c+d)/5
+stu=Student()
+print(stu.average(1,2,3,4,5))
